5896d93d2814686d02a8c87a6b21d841ec4ebab2
Edliner <edliner@proton.me>
2025-04-06 01:32:35 +0100

    GH actions

--- blog.sh
+++ blog.sh
@@ -1,12 +1,14 @@
-rm -f index.md
+rm -rf index.md web
+mkdir web
 for file in *.md; do
     date=$(echo "$file" | cut -d- -f1-3)
     path=$(echo "$file" | cut -d- -f4- | cut -d. -f1)
     title=$(head -1 "$file" | cut -d" " -f2-)
     echo $date
     echo $path
-    mkdir "$path"
-    set -x; pandoc "$file" --template "template.html" -V "ispost=ispost" -V "date=$date" -o "$path/index.html"; set +x
+    mkdir "web/$path"
+    set -x; pandoc "$file" --template "template.html" -V "ispost=ispost" -V "date=$date" -o "web/$path/index.html"; set +x
     echo "$date - <a href=\"$path/\">$title</a>" >> index.md
 done
-set -x; pandoc "index.md" --template "template.html" -o "index.html"; set +x
+set -x; pandoc "index.md" --template "template.html" -o "web/index.html"; set +x
+rm -f index.md
